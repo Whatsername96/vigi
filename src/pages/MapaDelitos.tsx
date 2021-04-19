31ff44ca13905b9e98e1215e02c2b8afd510f73d
Bruna Fernandes <brunafernandes035@gmail.com>
2021-04-19 19:16:54 -0300

    autocomplete do google funcionando e estilizado

--- src/pages/MapaDelitos.tsx
+++ src/pages/MapaDelitos.tsx
@@ -20,11 +20,11 @@ export default function MapaDelitos() {
         let status = Permission.askAsync(Permission.LOCATION);
         if ((await status).status !== 'granted') {
             setErrorMessage('Permissão negada, não é possível mostrar a localização');
-           
+
         } else {
             let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest });
-            const { latitude , longitude } = location.coords
-            let endereco = Location.reverseGeocodeAsync( {latitude, longitude} )
+            const { latitude, longitude } = location.coords
+            let endereco = Location.reverseGeocodeAsync({ latitude, longitude })
             setLongitude(longitude);
             setLatitude(latitude);
         }
@@ -32,109 +32,160 @@ export default function MapaDelitos() {
 
     getLocationAsync();
 
-return (
-    <View style={styles.container}>
+    return (
+        <View style={styles.container}>
 
-        <StatusBar
-            barStyle="light-content"
-            hidden={false}
-            backgroundColor="#394867"
-            translucent={false}
-        />
+            <StatusBar
+                barStyle="light-content"
+                hidden={false}
+                backgroundColor="#394867"
+                translucent={false}
+            />
 
-        <MapView
-            showsUserLocation={true}
-            followsUserLocation={true}
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            region={{
-                latitude: latitude,
-                longitude: longitude,
-                latitudeDelta: 0.002,
-                longitudeDelta: 0.002,
-            }}
-        >
-            <Marker
-                icon={markerAssalto}
-                coordinate={{
+            <MapView
+                showsUserLocation={true}
+                followsUserLocation={true}
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={{
                     latitude: latitude,
                     longitude: longitude,
+                    latitudeDelta: 0.002,
+                    longitudeDelta: 0.002,
                 }}
             >
-                <Callout tooltip={true}>
-                    <View style={styles.calloutContainer}>
-                        <Text style={styles.calloutTitle}>Assalto</Text>
-                        <Text style={styles.calloutText}>07/04/2021 - 18h</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                <Marker
+                    icon={markerAssalto}
+                    coordinate={{
+                        latitude: latitude,
+                        longitude: longitude,
+                    }}
+                >
+                    <Callout tooltip={true}>
+                        <View style={styles.calloutContainer}>
+                            <Text style={styles.calloutTitle}>Assalto</Text>
+                            <Text style={styles.calloutText}>07/04/2021 - 18h</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
 
-        <View style={styles.inputLocationView}>
             <GooglePlacesAutocomplete
                 placeholder='Digite o local'
                 textInputProps={{
                     placeholderTextColor: '#B4B3B3',
                     autoCapitalize: 'none',
-                    autoCorrect: false
+                    autoCorrect: false,
                 }}
-
                 styles={{
+                    container: {
+                        position: 'absolute',
+                        width: '100%',
+                        top: 10,
+                    },
+                    textInputContainer: {
+                        flex: 1,
+                        backgroundColor: 'transparent',
+                        marginHorizontal: 20,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                    },
                     textInput: {
+                        height: 60,
                         textAlign: 'left',
-                        fontSize: 20,
+                        fontSize: 18,
                         fontFamily: 'Abel_400Regular',
                         color: '#000',
+                        margin: 0,
+                        borderRadius: 6,
+                        paddingTop: 0,
+                        paddingBottom: 0,
+                        paddingLeft: 20,
+                        paddingRight: 20,
+                        padding: 0,
+                        elevation: 5,
+                        shadowColor: '#000',
+                        shadowOpacity: 0.1,
+                        shadowOffset: { x: 0, y: 0 },
+                        shadowRadius: 15,
+                    },
+
+                    listView: {
+                        borderWidth: 1,
+                        borderRadius: 6,
+                        borderColor: '#B4B3B3',
+                        backgroundColor: '#FFF',
+                        marginHorizontal: 20,
+                        marginVertical: 0,
+                        elevation: 5,
+                        shadowColor: '#000',
+                        shadowOpacity: 0.1,
+                        shadowOffset: { x: 0, y: 0 },
+                        shadowRadius: 15,
+                        zIndex: 1,
+                    },
+
+                    description: {
+                        fontSize: 16,
+                    },
+
+                    row: {
+                        height: 58,
+                        padding: 20,
+                        alignItems: 'center',
+                        borderWidth: 0.5,
+                        borderColor: '#B4B3B3'
                     }
+
                 }}
                 onPress={(data, details = null) => {
                     // 'details' is provided when fetchDetails = true
                     console.log(data, details);
                 }}
                 query={{
-                    key: 'key',
+                    key: 'my-key',
                     language: 'pt',
                 }}
                 fetchDetails
                 enablePoweredByContainer={false}
                 onFail={error => console.error(error)}
             />
+            <View style={styles.viewLocalUsuario}>
+                <RectButton onPress={() => getLocationAsync()}>
+                    <Image source={
+                        require('../images/input/local-usuario.png')
+                    } />
+                </RectButton>
+            </View>
 
-            <RectButton onPress={() => getLocationAsync()}>
-                <Image source={
-                    require('../images/input/local-usuario.png')
-                } />
-            </RectButton>
+            <View style={styles.containerButtons}>
+                <RectButton style={styles.btnSearch} onPress={() => alert('funcionando')}>
+                    <Text style={styles.btnText}>Consultar</Text>
+                </RectButton>
+
+                <RectButton style={styles.btnDenounce} onPress={() => alert('funcionando')}>
+                    <Text style={styles.btnText}>Denunciar</Text>
+                </RectButton>
+            </View>
+
+            <View style={styles.containerInfo}>
+                <RectButton onPress={() => setModal(true)}>
+                    <Image source={
+                        require('../images/header/info.png')
+                    } />
+                </RectButton>
+            </View>
+            <ModalApp show={modal}
+                close={() => setModal(false)}
+                title={'Informações'}
+                description={'Cada denúncia ficará disponível por 15 dias.'}
+                imgSuccess={false}
+                imgError={false}
+                btnBack={false}
+                route={''}
+            />
         </View>
-
-        <View style={styles.containerButtons}>
-            <RectButton style={styles.btnSearch} onPress={() => alert('funcionando')}>
-                <Text style={styles.btnText}>Consultar</Text>
-            </RectButton>
-
-            <RectButton style={styles.btnDenounce} onPress={() => alert('funcionando')}>
-                <Text style={styles.btnText}>Denunciar</Text>
-            </RectButton>
-        </View>
-
-        <View style={styles.containerInfo}>
-            <RectButton onPress={() => setModal(true)}>
-                <Image source={
-                    require('../images/header/info.png')
-                } />
-            </RectButton>
-        </View>
-        <ModalApp show={modal}
-            close={() => setModal(false)}
-            title={'Informações'}
-            description={'Cada denúncia ficará disponível por 15 dias.'}
-            imgSuccess={false}
-            imgError={false}
-            btnBack={false}
-            route={''}
-        />
-    </View>
-);
+    );
 }
 
 const styles = StyleSheet.create({
@@ -145,22 +196,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    inputLocationView: {
+    viewLocalUsuario: {
         position: 'absolute',
-        flex: 1,
-        width: '90%',
-        height: 60,
-        backgroundColor: '#FFF',
-        borderRadius: 6,
-        elevation: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        alignItems: 'center',
         top: 0,
-        margin: 10,
+        marginTop: 25,
+        marginRight: 35,
+        right: 0,
+        flex: 1,
         flexDirection: 'row',
-        paddingVertical: 10,
-        justifyContent: 'space-between',
+        alignItems: 'flex-end',
     },
 
     containerButtons: {
@@ -168,9 +212,11 @@ const styles = StyleSheet.create({
         flex: 1,
         top: 70,
         flexDirection: 'row',
-        width: '90%',
+        width: '100%',
         justifyContent: 'space-between',
         marginTop: 10,
+        paddingRight: 20,
+        paddingLeft: 20,
     },
 
     btnSearch: {
@@ -201,6 +247,7 @@ const styles = StyleSheet.create({
     map: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+        position: 'relative',
     },
 
     calloutContainer: {

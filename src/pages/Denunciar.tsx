285fb435565afe79617ffda641efbc5115c2ef40
Bruna Fernandes <brunafernandes035@gmail.com>
2021-04-26 21:17:15 -0300

    Tela de denúncias criada com autocomplete, picker dropdown e botao Salvar mostrando Modal de sucesso ao ser clicado

--- src/pages/Denunciar.tsx
+++ src/pages/Denunciar.tsx
@@ -4,9 +4,7 @@ import { TextInputMask } from 'react-native-masked-text'
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { Picker } from '@react-native-community/picker';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import Icon from 'react-native-vector-icons/Feather';
 
 import * as Location from 'expo-location';
 import * as Permission from 'expo-permissions';
@@ -43,152 +41,182 @@ export default function Denunciar() {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Faça sua denúncia</Text>
-            </View>
+        <ScrollView keyboardShouldPersistTaps={'handled'}>
+            <View style={styles.container}>
 
-            <GooglePlacesAutocomplete
-                placeholder='Digite o local'
-                textInputProps={{
-                    placeholderTextColor: '#B4B3B3',
-                    autoCapitalize: 'none',
-                    autoCorrect: false,
-                }}
-                styles={{
-                    container: {
-                        position: 'absolute',
-                        width: '100%',
-                        top: 80,
-                    },
-                    textInputContainer: {
-                        flex: 1,
-                        backgroundColor: 'transparent',
-                        marginHorizontal: 20,
-                        borderTopWidth: 0,
-                        borderBottomWidth: 0,
-                    },
-                    textInput: {
-                        height: 60,
-                        textAlign: 'left',
-                        fontSize: 18,
-                        fontFamily: 'Abel_400Regular',
-                        color: '#000',
-                        margin: 0,
-                        borderRadius: 6,
-                        paddingTop: 0,
-                        paddingBottom: 0,
-                        paddingLeft: 20,
-                        paddingRight: 52,
-                        padding: 0,
-                        elevation: 5,
-                        shadowColor: '#000',
-                        shadowOpacity: 0.1,
-                        shadowOffset: { x: 0, y: 0 },
-                        shadowRadius: 15,
-                    },
+                <View style={styles.viewTitle}>
+                    <Text style={styles.textTitle}>Faça sua denúncia</Text>
+                </View>
 
-                    listView: {
-                        borderWidth: 1,
-                        borderRadius: 6,
-                        borderColor: '#B4B3B3',
-                        backgroundColor: '#FFF',
-                        marginHorizontal: 20,
-                        marginVertical: 0,
-                        elevation: 5,
-                        shadowColor: '#000',
-                        shadowOpacity: 0.1,
-                        shadowOffset: { x: 0, y: 0 },
-                        shadowRadius: 15,
-                        zIndex: 1,
-                    },
-
-                    description: {
-                        fontSize: 16,
-                    },
-
-                    row: {
-                        height: 58,
-                        padding: 20,
-                        alignItems: 'center',
-                        borderWidth: 0.5,
-                        borderColor: '#B4B3B3'
-                    }
-
-                }}
-                onPress={(data, details = null) => {
-                    // 'details' is provided when fetchDetails = true
-                    console.log(details)
-
-                }}
-                query={{
-                    key: config.googleApi,
-                    language: 'pt-BR',
-                    components: 'country:br',
-                }}
-                nearbyPlacesAPI='GooglePlacesSearch'
-                fetchDetails
-                isRowScrollable
-                enableHighAccuracyLocation
-                enablePoweredByContainer={false}
-                onFail={error => console.error(error)}
-            />
-
-            <View style={styles.viewLocationUser}>
-                <RectButton onPress={() => getLocationAsync()}>
-                    <Image source={
-                        require('../images/input/local-usuario.png')
-                    } />
-                </RectButton>
-            </View>
-
-            <View style={styles.viewDateTime}>
-                <Text style={styles.textDateTime}>Data:</Text>
-                <TextInputMask
-                    placeholder={'__/__/___'}
-                    type={'datetime'}
-                    options={{
-                        format: 'DD/MM/YYYY'
+                <GooglePlacesAutocomplete
+                    placeholder='Digite o local'
+                    textInputProps={{
+                        placeholderTextColor: '#B4B3B3',
+                        autoCapitalize: 'none',
+                        autoCorrect: false,
                     }}
-                    style={styles.inputDate}
-                    keyboardType={'number-pad'}
-                    maxLength={10}
-                    value={date}
-                    onChangeText={text => { setDate(text) }}
-                />
-                <Text style={styles.textDateTime}>Hora:</Text>
-                <TextInputMask
-                    placeholder={'--:--'}
-                    type={'datetime'}
-                    options={{
-                        format: 'HH:MM'
+                    styles={{
+                        container: {
+                            position: 'absolute',
+                            width: '100%',
+                            top: 150,
+                        },
+                        textInputContainer: {
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            marginHorizontal: 20,
+                            borderTopWidth: 0,
+                            borderBottomWidth: 0,
+                        },
+                        textInput: {
+                            height: 60,
+                            textAlign: 'left',
+                            fontSize: 18,
+                            fontFamily: 'Abel_400Regular',
+                            color: '#000',
+                            margin: 0,
+                            borderRadius: 6,
+                            paddingTop: 0,
+                            paddingBottom: 0,
+                            paddingLeft: 20,
+                            paddingRight: 52,
+                            padding: 0,
+                            elevation: 5,
+                            shadowColor: '#000',
+                            shadowOpacity: 0.1,
+                            shadowOffset: { x: 0, y: 0 },
+                            shadowRadius: 15,
+                        },
+
+                        listView: {
+                            borderWidth: 1,
+                            borderRadius: 6,
+                            borderColor: '#B4B3B3',
+                            backgroundColor: '#FFF',
+                            marginHorizontal: 20,
+                            marginVertical: 0,
+                            elevation: 5,
+                            shadowColor: '#000',
+                            shadowOpacity: 0.1,
+                            shadowOffset: { x: 0, y: 0 },
+                            shadowRadius: 15,
+                            zIndex: 1,
+                        },
+
+                        description: {
+                            fontSize: 16,
+                        },
+
+                        row: {
+                            height: 58,
+                            padding: 20,
+                            alignItems: 'center',
+                            borderWidth: 0.5,
+                            borderColor: '#B4B3B3'
+                        }
+
                     }}
-                    style={styles.inputTime}
-                    keyboardType={'number-pad'}
-                    maxLength={6}
-                    value={time}
-                    onChangeText={text => { setTime(text) }}
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(details)
+
+                    }}
+                    query={{
+                        key: config.googleApi,
+                        language: 'pt-BR',
+                        components: 'country:br',
+                    }}
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    fetchDetails
+                    isRowScrollable
+                    enableHighAccuracyLocation
+                    enablePoweredByContainer={false}
+                    onFail={error => console.error(error)}
+                />
+
+                <View style={styles.viewLocationUser}>
+                    <RectButton onPress={() => getLocationAsync()}>
+                        <Image source={
+                            require('../images/input/local-usuario.png')
+                        } />
+                    </RectButton>
+                </View>
+
+                <View style={styles.viewDateTime}>
+                    <Text style={styles.textDateTime}>Data:</Text>
+                    <TextInputMask
+                        placeholder={'__/__/___'}
+                        type={'datetime'}
+                        options={{
+                            format: 'DD/MM/YYYY'
+                        }}
+                        style={styles.inputDate}
+                        keyboardType={'number-pad'}
+                        maxLength={10}
+                        value={date}
+                        onChangeText={text => { setDate(text) }}
+                    />
+                    <Text style={styles.textDateTime}>Hora:</Text>
+                    <TextInputMask
+                        placeholder={'--:--'}
+                        type={'datetime'}
+                        options={{
+                            format: 'HH:MM'
+                        }}
+                        style={styles.inputTime}
+                        keyboardType={'number-pad'}
+                        maxLength={6}
+                        value={time}
+                        onChangeText={text => { setTime(text) }}
+                    />
+                </View>
+
+                <View style={styles.viewPicker}>
+                    <DropDownPicker
+                        placeholder={'Selecione um tipo de delito...'}
+                        placeholderStyle={styles.pickerPlaceholder}
+                        items={[
+                            { label: 'Assalto', value: 'assalto', icon: () => <Image source={require('../images/icons-picker/assalto.png')} /> },
+                            { label: 'Ato Obsceno', value: 'ato-obsceno', icon: () => <Image source={require('../images/icons-picker/ato-obsceno.png')} /> },
+                            { label: 'Disparos', value: 'disparos', icon: () => <Image source={require('../images/icons-picker/disparos.png')} /> },
+                            { label: 'Furto', value: 'furto', icon: () => <Image source={require('../images/icons-picker/furto.png')} /> },
+                            { label: 'Homicídio', value: 'homicidio', icon: () => <Image source={require('../images/icons-picker/homicidio.png')} /> },
+                            { label: 'Lesão Corporal', value: 'lesao-corporal', icon: () => <Image source={require('../images/icons-picker/lesao-corporal.png')} /> },
+                            { label: 'Maus Tratos', value: 'maus-tratos', icon: () => <Image source={require('../images/icons-picker/maus-tratos.png')} /> },
+                            { label: 'Roubo', value: 'roubo', icon: () => <Image source={require('../images/icons-picker/roubo.png')} /> },
+                            { label: 'Sequestro', value: 'sequestro', icon: () => <Image source={require('../images/icons-picker/sequestro.png')} /> },
+                            { label: 'Tráfico', value: 'trafico', icon: () => <Image source={require('../images/icons-picker/trafico.png')} /> },
+                            { label: 'Usuários de Drogas', value: 'usuarios-drogas', icon: () => <Image source={require('../images/icons-picker/usuarios-drogas.png')} /> },
+                            { label: 'Vandalismo', value: 'vandalismo', icon: () => <Image source={require('../images/icons-picker/vandalismo.png')} /> },
+                            { label: 'Violação de Domicílio', value: 'violacao-domicilio', icon: () => <Image source={require('../images/icons-picker/violacao-domicilio.png')} /> },
+                            { label: 'Outros', value: 'outros', icon: () => <Image source={require('../images/icons-picker/outros.png')} /> },
+                        ]}
+                        defaultValue={''}
+                        containerStyle={styles.containerPicker}
+                        style={styles.dropdownPicker}
+                        itemStyle={styles.itemsPicker}
+                        labelStyle={styles.labelPicker}
+                        dropDownStyle={{ backgroundColor: '#FFF' }}
+                        onChangeItem={item => setSelectedValue(item)}
+                    />
+                </View>
+                <View style={styles.containerButtons}>
+                    <RectButton style={styles.btnDenounce} onPress={() => { setModal(true) }} >
+                        <Text style={styles.btnText}>Salvar</Text>
+                    </RectButton>
+                </View>
+                <ModalApp 
+                    show={modal}
+                    close={() => setModal(false)}
+                    title={undefined}
+                    description={'Denúncia feita com sucesso.'}
+                    imgSuccess={true}
+                    imgError={false}
+                    btnBack={false}
+                    route={''}
                 />
             </View>
-
-            <DropDownPicker
-                items={[
-                    { label: 'USA', value: 'usa', icon: () => <Icon name="flag" size={18} color="#900" />, hidden: true },
-                    { label: 'UK', value: 'uk', icon: () => <Icon name="flag" size={18} color="#900" /> },
-                    { label: 'France', value: 'france', icon: () => <Icon name="flag" size={18} color="#900" /> },
-                ]}
-                defaultValue={''}
-                containerStyle={{ height: 40 }}
-                style={{ backgroundColor: '#fafafa' }}
-                itemStyle={{
-                    justifyContent: 'flex-start'
-                }}
-                dropDownStyle={{ backgroundColor: '#fafafa' }}
-                onChangeItem={item => setSelectedValue(item)}
-            />
-
-            
-        </View>
+        </ScrollView>
     );
 }
 
@@ -201,8 +229,8 @@ const styles = StyleSheet.create({
 
     viewTitle: {
         alignItems: 'center',
-        marginTop: 20,
-        marginBottom: 80,
+        marginTop: 60,
+        marginBottom: 130,
     },
 
     textTitle: {
@@ -213,7 +241,7 @@ const styles = StyleSheet.create({
 
     viewLocationUser: {
         position: 'absolute',
-        top: 70,
+        top: 140,
         marginTop: 25,
         marginRight: 35,
         right: 0,
@@ -241,6 +269,7 @@ const styles = StyleSheet.create({
     },
 
     inputDate: {
+        height: 60,
         textAlign: 'center',
         backgroundColor: '#FFF',
         marginLeft: 10,
@@ -253,6 +282,7 @@ const styles = StyleSheet.create({
     },
 
     inputTime: {
+        height: 60,
         textAlign: 'center',
         backgroundColor: '#FFF',
         marginLeft: 10,
@@ -265,14 +295,69 @@ const styles = StyleSheet.create({
 
     viewPicker: {
         paddingHorizontal: 20,
+        marginTop: 40,
     },
 
-    picker: {
-        borderWidth: 1,
+    containerPicker: {
+        height: 60,
+    },
+
+    dropdownPicker: {
         backgroundColor: '#FFF',
+        borderRadius: 6,
         elevation: 5,
-        marginTop: 30,
+        height: 70,
         width: '100%',
-    }
+    },
+
+    pickerPlaceholder: {
+        fontFamily: 'Abel_400Regular',
+        fontSize: 20,
+        color: '#B4B3B3',
+    },
+
+    itemsPicker: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#D1D1D1',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+    },
+
+    labelPicker: {
+        justifyContent: 'center',
+        fontFamily: 'Abel_400Regular',
+        fontSize: 16,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        textAlign: 'left',
+    },
+
+    containerButtons: {
+        flex: 1,
+        width: '100%',
+        marginTop: 40,
+        paddingRight: 20,
+        paddingLeft: 20,
+        zIndex: 2,
+    },
+
+    btnDenounce: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#000',
+        borderRadius: 6,
+        justifyContent: 'center',
+        elevation: 5,
+    },
+
+    btnText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontFamily: 'Abel_400Regular',
+        color: '#FFF',
+    },
+
 
 })

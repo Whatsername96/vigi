d64556ef248fec7a4e3568c9076dd30eecd33b63
Bruna Fernandes <brunafernandes035@gmail.com>
2021-05-10 02:32:54 -0300

    funcao de remover as denuncias depois de 15 dias criado

--- backend/src/controllers/DelitosController.ts
+++ backend/src/controllers/DelitosController.ts
@@ -16,7 +16,7 @@ export default {
         return response.json(delitoView.renderMany(delitos));
     },
 
-    async create(request: Request, response: Response){
+    async create(request: Request, response: Response) {
         const {
             tipo_delito,
             data,
@@ -24,36 +24,58 @@ export default {
             latitude,
             longitude,
             descricao,
-         } = request.body;
-      
-         const delitosRepository = getRepository(Delito);
-      
-         const dados = { 
+        } = request.body;
+
+        const delitosRepository = getRepository(Delito);
+
+        const dados = {
             tipo_delito,
             data,
             hora,
             latitude,
             longitude,
             descricao,
-         };
+        };
 
-         const schema = Yup.object().shape({
+        const schema = Yup.object().shape({
             tipo_delito: Yup.string().required('Tipo de delito é obrigatório'),
             data: Yup.string().required('Data é obrigatório'),
             hora: Yup.string().required('Hora é obrigatório'),
             latitude: Yup.number().required('Latitude é obrigatório'),
             longitude: Yup.number().required('Longitude é obrigatório'),
             descricao: Yup.string().optional().nullable(),
-         });
+        });
 
-         await schema.validate(dados, {
-             abortEarly: false,
-         });
+        await schema.validate(dados, {
+            abortEarly: false,
+        });
 
-         const delito = delitosRepository.create(dados);
-      
-         await delitosRepository.save(delito);
-      
-         return response.status(201).json(delito);
-      }
+
+        const delito = delitosRepository.create(dados);
+
+        await delitosRepository.save(delito);
+
+        return response.status(201).json(delito);
+    },
+
+    async remove(request: Request, response: Response) {
+
+        const delitosRepository = getRepository(Delito);
+
+        const delitos = await delitosRepository.find();
+
+        delitos.forEach(async delito => {
+            let partesData = delito.data.split("-");
+            let date = new Date(parseInt(partesData[2]),
+                (parseInt(partesData[1]) - 1),
+                parseInt(partesData[0]));
+
+            let expirado = new Date();
+            expirado.setDate(expirado.getDate() - 15);
+            if (date < expirado) {
+                await delitosRepository.remove(delito);
+            }
+        });
+        return response.status(200).json({message: 'Registros expirados excluídos'});
     }
+}
